8b9efca495b05cd6bd6c3fe5f84e1dc3f29a8572
Your Name <you@example.com>
2026-04-16 19:52:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -27,7 +27,7 @@ Pair * createPair( char * key,  void * value) {
 
 long hash( char * key, long capacity) {
     unsigned long hash = 0;
-     char * ptr;
+    char * ptr;
     for (ptr = key; *ptr != '\0'; ptr++) {
         hash += hash*32 + tolower(*ptr);
     }
